a63306c783c5530d2eef1bd5d5e89fa0284a0d54
Ben Wolz <wolz@mit.edu>
2020-09-19 15:34:53 -0700

    redid javascript middleware

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,28 +1,36 @@
-const username = document.getElementById("username");
-const gameId = document.getElementById("gameId");
-const joinGameButton = document.getElementById("joinGame");
+//const joinGameButton = document.getElementById("joinGame");
 
 var socket = io();
 
 function joinGame() {
-    var requestJson = {"username": username.value, "gameId": gameId.value};
+    const username = document.getElementById("joinGameUsername");
+    const gameId = document.getElementById("gameId");
+    const requestJson = {"username": username.value, "gameId": gameId.value};
+
     console.log(`Attempting to join game ${gameId.value}`);
     socket.emit('userJoinGame', requestJson, error => {
         console.log(error);
         alert("Username and/or gameId is not valid.");
     });
 
-    window.location.href = '/facts';
+    let endOfURL = '/facts' + '?username='+username.value + '?gameId=' +gameId.value;
+    window.location.href = endOfURL;
 }
 
 function createGame() {
-    var requestJson = {"username": username.value, "gameId": gameId.value};
+    const createGameUsername = document.getElementById("createGameUsername");
+    // hypothetically should create a new game ID for us
+    const requestJson = {"username": createGameUsername.value, "gameId": gameId.value};
+
     console.log('Trying to create a game');
     console.log(requestJson);
+
     socket.emit('userCreateGame', requestJson, error => {
         console.log(error);
         alert("Username and/or gameId is not valid.");
     });
 
-    window.location.href = '/facts';
+    
+    let endOfURL = '/facts?username=' + createGameUsername.value;
+    window.location.href = endOfURL;
 }
