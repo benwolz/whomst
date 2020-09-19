e3e4ba3a9d14e398b93c5493b434ac5a367209f2
Grant Miller <gmmiller@mit.edu>
2020-09-19 14:36:25 -0700

    Added apis

--- src/app.js
+++ src/app.js
@@ -5,15 +5,22 @@ const http = require('http');
 const db = require('./db');
 const api = require('./routes/api.js');
 const socketIO = require('socket.io');
+const session = require('express-session');
 // const bodyParser = require('body-parser');
 // const passport = require('./passport');
 
 const Game = require('./schemas/game');
-const Player = require('./schemas/player')
+const Player = require('./schemas/player');
 
 const app = express();
 const server = http.createServer(app);
 var io = socketIO(server);
+
+app.use(session({
+  secret: 'session-secret',
+  resave: 'false',
+  saveUninitialized: 'true'
+}));
 
 app.use(express.static('public'));
 app.use('/api',api);
@@ -42,50 +49,72 @@ server.listen(process.env.PORT, () => {
   console.log(`Listening on localhost:${process.env.PORT}`);
 });
 
+
+
+const rooms = [];
+
 io.on('connection', (socket) => {
   console.log('socket connected');
 
   socket.on('userCreateGame', (req) => {
-    console.log("attempting to create game");
-    Game.find().distinct('game_id', (error, ids) => {
-      var gameId = Math.floor(Math.random()*900000)+100000; // Random 6 digit number
-      while(ids.includes(gameId)) {
-        gameId = Math.floor(Math.random()*900000)+100000;
-      }
-      let game = new Game({
-        game_id: gameId,
-        host_id: socket.id,
-        host_name: req.username,
-        players: [socket.id]
-      });
-      game.save();
-      console.log(`Game created with game pin ${gameId}`);
-    });
+    var gameId = Math.floor(Math.random()*900000)+100000; // Random 6 digit number
+    while (rooms.includes(gameId)) {
+      gameId = Math.floor(Math.random()*900000)+100000;
+    }
+    
+    socket.room = gameId;
+    rooms.push(gameId);
+    socket.username = req.username;
+    socket.join(gameId);
+
+    // console.log("attempting to create game");
+    // Game.find().distinct('game_id', (error, ids) => {
+    //   var gameId = Math.floor(Math.random()*900000)+100000; // Random 6 digit number
+    //   while(ids.includes(gameId)) {
+    //     gameId = Math.floor(Math.random()*900000)+100000;
+    //   }
+    //   let game = new Game({
+    //     game_id: gameId,
+    //     host_id: socket.id,
+    //     host_name: req.username,
+    //     players: [socket.id]
+    //   });
+    //   game.save();
+    //   console.log(`Game created with game pin ${gameId}`);
+    // });
   });
 
   socket.on('userJoinGame', (req) => {
-    Game.findOne({game_id: req.gameId }).then(game => {
-      if (game && !game.players.includes(socket.id)) {
-        let player = new Player({ // Create a new player
-          score: 0,
-          user_id: socket.id,
-          username: req.username,
-          isHost: game.game_id === req.gameId,
-          facts: [] // Need to add facts
-        });
-        player.save();
-        console.log(`User ${req.username} joined game with id ${req.gameId}`);
+    if (rooms.includes(req.gameId)) {
+      socket.join(req.gameId);
+      socket.to(req.gameId).emit("newUserJoined", req.username);
+    } 
 
-        game.players.push(socket.id);
-        game.save();
-      } else {
-        if(game && game.players.includes(socket.id)) {
-          socket.emit("exception", {errorMessage: "Player already in the game"}); // This doesn't quite work yet
-        } else { // Need to figure out how to send information back to client
-          socket.emit("exception", {errorMessage: "Game pin does not exist"});
-        }
-      }
-    });
+    // Game.findOne({game_id: req.gameId }).then(game => {
+    //   if (game && !game.players.includes(socket.id)) {
+        // let player = new Player({ // Create a new player
+        //   score: 0,
+        //   user_id: socket.id,
+        //   username: req.username,
+        //   isHost: game.game_id === req.gameId,
+        //   facts: [] // Need to add facts
+        // });
+        // player.save();
+        // console.log(`User ${req.username} joined game with id ${req.gameId}`);
+
+    //     game.players.push(socket.id);
+    //     game.save();
+    //   } else {
+    //     if(game && game.players.includes(socket.id)) {
+    //       socket.emit("exception", {errorMessage: "Player already in the game"}); // This doesn't quite work yet
+    //     } else { // Need to figure out how to send information back to client
+    //       socket.emit("exception", {errorMessage: "Game pin does not exist"});
+    //     }
+    //   }
+    // });
+
+    // add endpoint for leaving games
+    // need to figure out how to send data back to the client
 
   })
 
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,82 @@
 const express = require("express");
 const router = express.Router();
 
+const Game = require('../schemas/game');
+const Player = require('../schemas/player');
+
 router.get('/test', (req, res) => {
   console.log('testing api works');
+  console.log(req.session.id);
   res.send({});
 });
+
+router.post('/create-game', (req, res) => {
+  console.log("attempting to create game");
+    Game.find().distinct('game_id', (error, ids) => {
+      var gameId = Math.floor(Math.random()*900000)+100000; // Random 6 digit number
+      while(ids.includes(gameId)) {
+        gameId = Math.floor(Math.random()*900000)+100000;
+      }
+      let game = new Game({
+        game_id: gameId,
+        host_id: req.session.id,
+        host_name: req.username,
+        players: [req.session.id]
+      });
+      game.save();
+      console.log(`Game created with game pin ${gameId}`);
+
+      let player = new Player({ // Create a new player
+        score: 0,
+        user_id: req.session.id,
+        username: req.username,
+        isHost: true,
+        facts: [] 
+      });
+      player.save();
+      console.log(`User ${req.username} joined game with id ${req.gameId}`);
+
+      
+      res.send({
+        status: 'ok',
+        gameId: gameId
+      });
+    });
+});
+
+router.post('/join-game', (req, res) => {
+  console.log("Attempting to join a game");
+  Game.findOne({game_id: req.gameId }).then(game => {
+    if (game && !game.players.includes(req.session.id)) {
+      let player = new Player({ // Create a new player
+        score: 0,
+        user_id: req.session.id,
+        username: req.username,
+        isHost: game.game_id === req.gameId,
+        facts: [] // Need to add facts
+      });
+      player.save();
+      console.log(`User ${req.username} joined game with id ${req.gameId}`);
+
+      game.players.push(req.session.id);
+      game.save();
+    } else {
+      if(game && game.players.includes(req.session.id)) {
+        res.send({
+          status: 'failed',
+          errorMessage: 'Player already in game',
+          error: 0
+        })
+      } else { // Need to figure out how to send information back to client
+        res.send({
+          status: 'failed',
+          errorMessage: 'Game does not exist',
+          error: 1
+        })
+      }
+    }
+  });
+});
+
 
 module.exports = router;
