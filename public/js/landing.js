f676df38743dfff2638e53f4d4c457eb5218e3f0
Peyton Greve <pgreve@mit.edu>
2020-09-19 00:33:31 -0700

    added functions to handle button clicks on landing

---
+++ public/js/landing.js
@@ -0,0 +1,23 @@
+const username = document.getElementById("username");
+const gameId = document.getElementById("gameId");
+const joinGameButton = document.getElementById("joinGame");
+
+var socket = io();
+
+function joinGame() {
+    var requestJson = {"username": username.value, "gameId": gameId.value, "isHost": false};
+
+    socket.emit('userJoinGame', requestJson, error => {
+        console.log(error);
+        alert("Username and/or gameId is not valid.");
+    });
+}
+
+function createGame() {
+    var requestJson = {"username": username.value, "gameId": gameId.value, "isHost": true};
+
+    socket.emit('userCreateGame', requestJson, error => {
+        console.log(error);
+        alert("Username and/or gameId is not valid.");
+    });
+}
