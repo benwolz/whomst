5abf5c1ad1fbd8c8886623dc2ac66c77f687ceff
Grant Miller <gmmiller@mit.edu>
2020-09-18 18:50:08 -0700

    Added eslint

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-"use strict";
+var express = require('express');
+var app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('asldkfj World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
